8b5431c714667455535099fca2a8846f65f19a0d
Mostafa ElKholy <your-email@example.com>
2025-05-15 14:18:39 +0300

    Update OpenRouter API key and adjust API endpoint in ChatAiController

--- app/Http/Controllers/ChatAiController.php
+++ app/Http/Controllers/ChatAiController.php
@@ -10,11 +10,11 @@ class ChatAiController extends Controller
     public function ask(Request $request)
     {
        $result = Http::withHeaders([
-    'Authorization' => 'Bearer ' . env('OPENROUTER_API_KEY'),
+    'Authorization' => 'Bearer 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' ,
     'HTTP-Referer' => 'https://university-hti-project.vercel.app/', // اختياري
     'X-Title' => 'University AI Assistant', // اختياري
     'Content-Type' => 'application/json',
-])->post('https://openrouter.ai/api/v1/chat/completions', [
+])->post('https://openrouter.ai/api/v1', [
     'model' => 'deepseek/deepseek-r1:free',
     'messages' => [
         [
