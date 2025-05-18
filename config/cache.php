65842a33d3161156ad253eec851ad9173ebfeeea
Mostafa ElKholy <your-email@example.com>
2025-05-18 16:41:16 +0300

    Add CORS configuration to cache.php for API requests

--- config/cache.php
+++ config/cache.php
@@ -30,6 +30,16 @@ return [
     |                    "redis", "dynamodb", "octane", "null"
     |
     */
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+'allowed_methods' => ['*'],
+
+'allowed_origins' => ['http://localhost:5173'],
+
+'allowed_headers' => ['*'],
+
+'supports_credentials' => true,
+
 
     'stores' => [
 
